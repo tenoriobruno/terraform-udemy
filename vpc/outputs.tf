cc1bce1caec0bc024db78af418b16f15867fd8f2
Bruno Tenório <bruno.tenorio1@gmail.com>
2024-10-12 09:43:21 -0300

    add subnet id and security group id outputs

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -7,3 +7,13 @@ output "bucket_arn" {
   description = "Nome da bucket criada com Terraform"
   value       = aws_s3_bucket.remote_state_btenorio.arn
 }
+
+output "subnet_id"{
+  description = "ID da subnet"
+  value = aws_subnet.subnet.id
+}
+
+output "security_group_id"{
+  description = "ID da securty group"
+  value = aws_security_group.security_group.id
+}
