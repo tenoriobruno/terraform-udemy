36f193a42178a44afddd79b311e3234b8f5bc1d2
Bruno Tenório <bruno.tenorio1@gmail.com>
2024-10-12 10:45:01 -0300

    ec2 remote state

---
+++ ec2/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.70.0"
+    }
+  }
+  backend "s3" {
+    bucket = "remote-state-btenorio"
+    key    = "remote-state-ec2.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+  default_tags {
+    tags = {
+      owner      = "bruno"
+      managed-by = "terraform"
+    }
+  }
+}
