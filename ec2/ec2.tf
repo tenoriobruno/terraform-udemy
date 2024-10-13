717a591d2ca092f1d2dd96d4495765391ec74b37
Bruno Tenório <bruno.tenorio1@gmail.com>
2024-10-13 20:25:28 -0300

    só use provisioners em último caso

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -13,6 +13,7 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids      = [data.terraform_remote_state.remote_data_source_vpc.outputs.security_group_id]
   associate_public_ip_address = true
 
+  #use provisioner só em último caso. Use user_data para enviar arquivos para a ec2
   provisioner "local-exec" {
     command = "echo ${self.private_ip} >> private_ips.txt"
   }
